3fb8dbb9a1548e2134e6f22f159aa98b614bb264
dréams <151709665+esaaramadhan@users.noreply.github.com>
2024-04-16 18:17:58 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Project Paradox kernel by @alcindyn 
+kernel.string=Project Paradox kernel by @dreams 
 do.devicecheck=1
 do.modules=0
 do.systemless=1
